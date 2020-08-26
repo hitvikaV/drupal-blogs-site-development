7d9a0386f5e6754f096f779ffdb2a442109bfa5c
hitvikaV <hitvika.v@opensenselabs.com>
2020-08-26 21:34:25 +0530

    new theme

--- Assets/olympos-master/lib/main.js
+++ Assets/olympos-master/lib/main.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function($) {
-    
+    // $('#block-mythemes-main-menu li').hover(function(){
+    //     $(this).find('.sub-menu').toggle()
+    //     console.log('hiii')
+    // })
 });
---
+++ web/themes/custome/mythemes/js/main.js
@@ -0,0 +1,74 @@
+jQuery(document).ready(function($) {
+    $('#block-mythemes-main-menu li').hover(function(){
+        $(this).find('.sub-menu').toggle()
+        console.log('hiii')
+    })
+
+    $(".my-slider").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      infinite: true
+    });
+    $button.removeAttr('aria-label');
+
+    $(".test").find("a").addClass('fa fa-bars')
+    // $(function() {
+
+    //   var inWrap = $('.inner-wrapper'),
+    //   $slide = $('.slide');
+    
+    
+    //   function slideNext() {
+    
+    //     inWrap.animate({left: '-200%'}, 200, function() {
+      
+    //       inWrap.css('left', '-100%');
+      
+    //       $('.slide').last().after($('.slide').first());
+      
+    //     });
+      
+    //   }
+    
+    
+    //    //Enabling auto scroll
+    //    sliderInterval = setInterval(slideNext, 4000);
+    
+    
+    
+    //   $('.prev').on('click', function() {
+    
+    //     inWrap.animate({left: '0%'}, 200, function() {
+    
+    //       inWrap.css('left', '-100%');
+    
+    //       $('.slide').first().before($('.slide').last());
+    
+    //     });
+    //   });
+    
+    
+    //   $('.next').on('click', function() {
+    
+    //     clearInterval(sliderInterval);
+    
+    //     slideNext();
+    //     console.log($slide)
+    
+    //   });
+    
+    
+    // });
+    // $('.my-slider').slick({
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
+    // });
+      
+});
+
+
+
